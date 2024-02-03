f50066f47dd1e7a921881a193b4cb0ba80921bc6
SeviRanWaru <149356832+SeviRanWaru@users.noreply.github.com>
2024-02-03 19:44:51 +0530

    end of 1st day

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ console.log(`I'm fan of Samsung`);
 
 window.alert('This an alert. Your device in very danger')
 */
+
 /*
 document.getElementById('myH1').textContent = 'Hello';
 document.getElementById('myP').textContent = 'I like pizza';
@@ -11,20 +12,50 @@ document.getElementById('myP').textContent = 'I like pizza';
 
 //this is just a comment
 
+
+
 /*
 let x;
 x = '123';
 console.log(x);
 */
 
-let insurance = 35;
-let oil = 15;
-let gas = 20;
-let maintaince = 25;
-let brand = 'nissan';
 
-console.log(typeof brand);
-console.log(`Your insuerance cost is $${insurance}`);
-console.log(`Your oil cost is $${oil}`);
-console.log(`Your gas cost is ${gas}`);
-console.log(`Your maintaince cost is $${maintaince}`);
+
+// let insurance = 35;
+// let oil = 15;
+// let gas = "🥲";
+// let maintaince = 25;
+// let brand = 'nissan';
+
+// console.log(typeof gas);
+// console.log(`Your insuerance cost is $${insurance}`);
+// console.log(`Your oil cost is $${oil}`);
+// console.log(`Your gas cost is ${gas}`);
+// console.log(`Your maintaince cost is $${maintaince}`);
+
+
+
+
+// let online = true;
+// let forSale = true;
+// let isStudent = false;
+
+// console.log(`bro is online: ${online}`);
+// console.log(`is this car for ${forSale}`);
+// console.log(`enrolled: ${isStudent}`);
+
+
+
+
+
+// let fullname = "sevindu warusawithana";
+// let age = 21;
+// let isStudent = true;
+
+// document.getElementById("p1").textContent = `your name is ${fullname}`;
+// document.getElementById(`p2`).textContent = `your age is ${age}`;
+// document.getElementById(`p3`).textContent = isStudent;
+ 
+
+
