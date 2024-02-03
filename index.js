ae90a3ca3c5bfcf212dea5d8929c462a924286ad
SeviRanWaru <149356832+SeviRanWaru@users.noreply.github.com>
2024-02-03 18:23:53 +0530

    3rd commit

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
+/*
 console.log(`Hello`);
 console.log(`I'm fan of Samsung`);
 
 window.alert('This an alert. Your device in very danger')
+*/
+/*
+document.getElementById('myH1').textContent = 'Hello';
+document.getElementById('myP').textContent = 'I like pizza';
+*/
 
 //this is just a comment
+
+/*
+let x;
+x = '123';
+console.log(x);
+*/
+
+let insurance = 35;
+let oil = 15;
+let gas = 20;
+let maintaince = 25;
+let brand = 'nissan';
+
+console.log(typeof brand);
+console.log(`Your insuerance cost is $${insurance}`);
+console.log(`Your oil cost is $${oil}`);
+console.log(`Your gas cost is ${gas}`);
+console.log(`Your maintaince cost is $${maintaince}`);
