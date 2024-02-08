fe57db5c813102df4cd14655b78e4485dfd6e1a3
SeviRanWaru <149356832+SeviRanWaru@users.noreply.github.com>
2024-02-09 05:11:31 +0530

    random number generator

--- index.js
+++ index.js
@@ -188,4 +188,40 @@ console.log(x);
 // let max = Math.max(x, y, z);
 // let min = Math.min(x, y, z);
 
-// console.log(min);
+// console.log(min);.
+
+
+
+
+
+/* RANDOM NUMBER GENERATOR */
+
+// const min = 50;
+// const max = 100;
+
+// const randomNum = Math.floor(Math.random() * (max-min) + min);
+
+// console.log(randomNum);
+
+
+
+
+const btn = document.getElementById(`btn`);
+const lab1 = document.getElementById(`lab1`);
+const lab2 = document.getElementById(`lab2`);
+const lab3 = document.getElementById(`lab3`);
+const min = 1;
+const max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+btn.onclick = function() {
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    lab1.textContent = randomNum1;
+    lab2.textContent = randomNum2;
+    lab3.textContent = randomNum3;
+
+}
